5ce58e62d2a275168f27e63f33267dbfbcbbc0f7
Joxit <joxit972@gmail.com>
2020-07-28 11:13:15 +0200

    Add new icons and update old one

--- src/layers.js
+++ src/layers.js
@@ -2,7 +2,7 @@ const commonPoi = {
   "type": "symbol",
   "source-layer": "poi",
   "layout": {
-    "icon-image": "{class}_11",
+    "icon-image": "indoorequal-{class}",
     "text-anchor": "top",
     "text-field": "{name:latin}\n{name:nonlatin}",
     "text-max-width": 9,
@@ -233,7 +233,7 @@ export const layers = [
     ],
     "layout": {
       ...commonPoi.layout,
-      "icon-image": "{subclass}_11"
+      "icon-image": "indoorequal-{subclass}"
     }
   },
   {
