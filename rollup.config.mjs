cd554f03af21ba546f131bfc9f21f9d2648a479c
François de Metz <francois@2metz.fr>
2023-06-10 15:55:40 +0200

    Switch to @rollup/plugin-terser.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,6 @@
 import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 import commonjs from '@rollup/plugin-commonjs';
 import pkg from './package.json' assert { type: 'json' };
 
