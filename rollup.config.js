34ff2ca2b4540418d953d1b56f38c96fd6d0b356
François de Metz <francois@2metz.fr>
2026-01-02 11:10:11 +0100

    Fix the build.

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 import terser from '@rollup/plugin-terser';
 import commonjs from '@rollup/plugin-commonjs';
-import pkg from './package.json' assert { type: 'json' };
+import pkg from './package.json' with { type: 'json' };
 
 const input = 'src/index.js';
 
